affb0a3c788c8dcddff6eda719b60cf871289e3e
Jordan10980 <jordandohou11@gmail.com>
2023-01-28 12:42:48 +0100

    ajout du module de connexion

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 	return (
 		<div>
 			<Navbar/>
-			<Inscription/>
+			<Contact/>
 			<Footer/>
 		</div>
 	);
--- src/Connexion.js
+++ src/Connexion.js
@@ -4,7 +4,42 @@ import "./Connexion.css"
 
 const Connexion=()=>{
 
-
+    const [formData, setFormData] = useState({
+        mailconnect: '',
+        mdpconnect: '',
+        rememberme: '',
+      });
+      const [response, setResponse] = useState(null);
+      
+      
+      const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((formData) => ({ ...formData, [name]: value }));
+      };
+      
+      const handleSubmit = (event) => {
+        event.preventDefault();
+      
+        fetch("http://localhost/php_saes4/connexion.php", {
+          method: 'POST',
+          body: JSON.stringify({
+            data: formData,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+          .then((response) => response.text())
+          .then((data) => {
+            setResponse(data);
+            if (data.includes('Bravo vous êtes connecté !')) {
+                setResponse(<p class="valid">Bravo vous êtes inscrit.</p>);
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      };    
     
     return( 
         <>
@@ -14,24 +49,19 @@ const Connexion=()=>{
     </section>
 
     <section className="connexion">
-        <form>
+        <form onSubmit={handleSubmit}>
 
-            <input type="email" name="mailconnect" placeholder="Mail"/>
+            <input type="email" name="mailconnect" placeholder="Mail" onChange={handleChange}/>
             <br /><br />
-            <input type="password" name="mdpconnect" placeholder="Mot de passe" />
+            <input type="password" name="mdpconnect" placeholder="Mot de passe" onChange={handleChange}/>
             <br /><br /> <br /><br />
-            <input type="checkbox" name="rememberme" id="remembercheckbox" /><label class="label" for="remembercheckbox">Se souvenir de moi </label>
+            <input type="checkbox" name="rememberme" id="remembercheckbox" onChange={handleChange}/><label class="label" for="remembercheckbox">Se souvenir de moi </label>
             <br /><br /> <br />
             <input type="submit" name="formconnexion" id='btn' value="Se connecter !"/>
         </form>
 
-        
-
-
+        {response ? <p>{response}</p> : null}
         <br /><br />    
-
-            
-            <br /><br /><br /><br /><br /><br /><br />
 
     </section>
 
